719d698fedc26bacf2d5d4d10bb1151b51d16877
unknown <dima.konovko@mail.ru>
2019-11-06 11:15:56 +0300

    development add: empty projects

---
+++ FakerApp/Faker/Faker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faker
+{
+    public class Faker
+    {
+        private Dictionary<Type, List<object>> innerTypes;
+
+        public T Create<T>()
+        {
+            innerTypes = new Dictionary<Type, List<object>>();
+            T obj = (T)CreateDTO(typeof(T));
+            return obj;
+        }
+    }
+}
---
+++ FakerApp/Generators/ByteGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generators
+{
+    public class ByteGenerator : IGenerator
+    {
+        public object Generate()
+        {
+            var random = new Random();
+            byte result = 0;
+            while (result == 0)
+            {
+                result = (byte)random.Next(byte.MinValue, byte.MaxValue);
+            }
+            return result;
+        }
+
+        public Type GeneratedType()
+        {
+            return typeof(byte);
+        }
+    }
+}
