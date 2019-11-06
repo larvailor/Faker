71f9bbda73572d1bdb44506d01679673482306fb
unknown <dima.konovko@mail.ru>
2019-11-06 10:21:15 +0300

    development add: empty projects
    Faker      - DTO generator
    FakerApp   - an example of how Faker should be used
    FakerTests - unit tests for Faker

---
+++ FakerApp/FakerTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FakerTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FakerTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("984e79bc-6323-4331-8dc8-94109f03adf9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
