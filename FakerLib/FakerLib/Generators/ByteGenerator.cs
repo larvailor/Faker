4dc0c26d16c34135d984a5f9c2afefd5b44ed7f4
unknown <dima.konovko@mail.ru>
2019-11-13 11:36:22 +0300

    development add: IGenerator and ByteGenerator

---
+++ FakerLib/FakerLib/Generators/ByteGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakerLib.Generators
+{
+    class ByteGenerator : IGenerator
+    {
+        public object Generate()
+        {
+            var random = new Random();
+            byte result = 0;
+            while (result == 0)
+            {
+                result = (byte)random.Next(byte.MinValue, byte.MaxValue);
+            }
+            return result;
+        }
+
+
+
+        public Type GeneratedType()
+        {
+            return typeof(byte);
+        }
+    }
+}
