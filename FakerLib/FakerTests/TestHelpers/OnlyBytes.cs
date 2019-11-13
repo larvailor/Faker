9dcb6817046e9d79dacf816680f1390e398a0634
unknown <dima.konovko@mail.ru>
2019-11-13 11:50:38 +0300

    development add: FakerTests project with HelperClasses

---
+++ FakerLib/FakerTests/TestHelpers/OnlyBytes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakerTests.TestHelpers
+{
+  
+    public class OnlyBytes
+    {
+        public byte x;
+        public byte y;
+
+        public byte z { set; get; }
+
+        public OnlyBytes(byte zxc) { }
+    }
+}
