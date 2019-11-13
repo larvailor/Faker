d7db9e667caba5a2fe4067d176a7d71fd708e345
unknown <dima.konovko@mail.ru>
2019-11-13 17:30:57 +0300

    development add: new tests

--- FakerLib/FakerTests/FakerTests.cs
+++ FakerLib/FakerTests/FakerTests.cs
@@ -9,13 +9,63 @@ namespace FakerTests
     public class FakerTests
     {
         [TestMethod]
-        public void GenerateClassWithOnlyBytes()
+        public void ClassWithOnlyBytes()
         {
             Faker faker = new Faker();
             OnlyBytes onlyBytes = faker.Create<OnlyBytes>();
             Assert.IsTrue(onlyBytes.x != 0);
             Assert.IsTrue(onlyBytes.y != 0);
             Assert.IsTrue(onlyBytes.z != 0);
+        }
+
+
+
+        [TestMethod]
+        public void ClassWithOnlyInt()
+        {
+            Faker faker = new Faker();
+            OnlyInt onlyInt = faker.Create<OnlyInt>();
+            Assert.IsTrue(onlyInt.x != 0);
+            Assert.IsTrue(onlyInt.y != 0);
+            Assert.IsTrue(onlyInt.z != 0);
+        }
+
+
+
+        [TestMethod]
+        public void ClassWithOnlyShort()
+        {
+            Faker faker = new Faker();
+            OnlyShort onlyShort = faker.Create<OnlyShort>();
+            Assert.IsTrue(onlyShort.x != 0);
+            Assert.IsTrue(onlyShort.y != 0);
+            Assert.IsTrue(onlyShort.z != 0);
+        }
+
+
+
+        [TestMethod]
+        public void ClassWithAllTypes()
+        {
+            Faker faker = new Faker();
+            AllTypes allTypes = faker.Create<AllTypes>();
+            Assert.IsTrue(allTypes.X != 0);
+            Assert.IsTrue(allTypes.Y != 0);
+            Assert.IsTrue(allTypes.Z != 0);
+        }
+
+
+
+        [TestMethod]
+        public void ClassWithInnerClass()
+        {
+            Faker faker = new Faker();
+            TwoClasses twoClasses = faker.Create<TwoClasses>();
+            Assert.IsTrue(twoClasses.x != 0);
+            Assert.IsTrue(twoClasses.allTypes.X != 0);
+            Assert.IsTrue(twoClasses.allTypes.Y != 0);
+            Assert.IsTrue(twoClasses.allTypes.Z != 0);
+
         }
     }
 }
---
+++ FakerLib/FakerTests/TestHelpers/OnlyShort.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakerTests.TestHelpers
+{
+    public class OnlyShort
+    {
+        public short x;
+        public short y;
+
+        public short z { set; get; }
+
+        public OnlyShort(short zxc) { }
+    }
+}
